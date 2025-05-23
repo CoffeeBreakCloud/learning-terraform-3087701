58f13ccc37d1712a6ccec2da7db5d7979e1c8c96
CoffeeBreakCloud <158502962+CoffeeBreakCloud@users.noreply.github.com>
2025-05-23 16:01:32 -0600

    sG

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -43,7 +43,7 @@ resource  "aws_security_group_rule" "blog_http_in"{
   from_port = 80
   to_port= 80
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -54,7 +54,7 @@ resource  "aws_security_group_rule" "blog_https_in"{
   from_port = 443
   to_port= 443
   protocol = "tcp"
-  cidr_blocks = ["0.0.0.0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -66,7 +66,7 @@ resource  "aws_security_group_rule" "blog_everithing_out"{
   from_port = 0
   to_port= 0
   protocol = "-1"
-  cidr_blocks = ["0.0.0.0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
