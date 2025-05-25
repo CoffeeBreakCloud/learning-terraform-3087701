5a3aa3f0cb12734cce4334050f8ccfa7a38580e2
Ivan Chavez <ivanchavez@Ivans-MacBook-Pro-2.local>
2025-05-25 14:32:50 -0600

    vpc

--- main.tf
+++ main.tf
@@ -19,19 +19,19 @@ data "aws_vpc" "default" {
 }
 
 
-module "bloc_vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "Dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
 
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
@@ -43,19 +43,62 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = module.bloc_vpc.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learn Terraform"
   }
 }
 
+
+
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+
+  # Security Group
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = {
+    ex-instance = {
+      name_prefix = "h1"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+      target_id   = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Development"
+    Project     = "Example"
+  }
+
+
+  listeners = {
+    ex-http-https-redirect = {
+      port          = 80
+      protocol      = "HTTP"
+      target_groups = 0
+
+    }
+
+  }
+
+}
+
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.0"
   name    = "blog_new"
 
-  vpc_id              = module.bloc_vpc.default_vpc_id
+  vpc_id              = module.blog_vpc.default_vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
